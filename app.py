2a73c8abd6faab0ce7c7373de3423b008ebe99a0
Subham Swain <subhamswain181@gmail.com>
2026-02-13 09:54:29 +0530

    update

--- app.py
+++ app.py
@@ -10,13 +10,23 @@ st.markdown("""
         }
         iframe {
             width: 100% !important;
-            height: 100% !important;
+            height: 100vh !important;
         }
     </style>
 """, unsafe_allow_html=True)
+
+# Hide Streamlit UI
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Load HTML
 with open("start.html", "r", encoding="utf-8") as f:
     html_content = f.read()
 
-st.components.v1.html(html_content, height=900, scrolling=True)
+st.components.v1.html(html_content, height=1000, scrolling=True)
